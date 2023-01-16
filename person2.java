3d0f750436528a1a800e721909a28ef56d31e39a
NaifHagatulah <naifhagatulah@hotmail.com>
2023-01-16 11:17:50 +0100

    updated all the code now i can work from home

--- person2.java
+++ person2.java
@@ -4,102 +4,90 @@ import java.util.Scanner;
 
 import javax.swing.text.html.HTMLDocument.RunElement;
 
-public class person2 
-{
- public static void main(String[] args) 
-{
-  Scanner input = new Scanner(System.in);
-  String math = input.nextLine();
-  String[] phrase = math.split("\\s+");
-  StringBuilder phraseMut = new StringBuilder(math);
- 
-  for(int i = 0; i < phrase.length; i++)
-  {
-    phraseMut.append(phrase[i]);
+public class person2 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String math = input.nextLine();
+    String[] phrase = math.split("\\s+");
+    StringBuilder phraseMut = new StringBuilder();
+
+    for (int i = 0; i < phrase.length; i++) {
+      phraseMut.append(phrase[i]);
+    }
+
+    int[] index = findIndexOfParenthesis(phraseMut);
+    System.out.println(phraseMut.substring(index[0], index[1]));
+    int findOperations = findOperations(phraseMut);
+    System.out.println(findOperations);
   }
 
-  int[] index = findIndexOfParenthesis(phraseMut);
-  System.out.println(phraseMut.substring(index[0], index[1]));
-  int findOperations = findOperations(phraseMut);
-  System.out.println(findOperations);
-}
-
-public static int[] findIndexOfParenthesis(StringBuilder phrase)
-{
-  int[] index = new int[2];
-  for(int i = 0; i < phrase.length(); i++)
-  {
-    char ph = phrase.charAt(i);
-    if(phrase.charAt(i) == 40)
-      index[0] = i + 1;  //start parentheses
-    if(phrase.charAt(i) == 41)
-      {
-        index[1] = i ;    //end parantheses
+  public static int[] findIndexOfParenthesis(StringBuilder phrase) {
+    int[] index = new int[2];
+    for (int i = 0; i < phrase.length(); i++) {
+      char ph = phrase.charAt(i);
+      if (phrase.charAt(i) == 40)
+        index[0] = i + 1; // start parentheses
+      if (phrase.charAt(i) == 41) {
+        index[1] = i; // end parantheses
         return index;
       }
-  }
-
-  return index;
-}
-
-public static List<Integer> findIndexOfOperation(char[] subPhrase)
-{
-  List<Integer> index = new ArrayList();
-
-  for(int i = 0; i < subPhrase.length; i++)
-  {
-    System.out.print(subPhrase[i]);
-    if(subPhrase[i] == 43 || subPhrase[i] == 42 || subPhrase[i] == 45 || subPhrase[i] == 47 )
-    {
-      index.add(i);
     }
+
+    return index;
   }
-  return index;
-}
 
-public static int findOperations(StringBuilder phrase)
-{
-  
- 
-  int[] index = findIndexOfParenthesis(phrase);
-  phrase.chars();
-  char[] test = new char[index[1] - index[0]]; // filling the char array with the values
-  phrase.getChars(index[0], index[1], test, 0); //get the value need correct index for the intergers
-  List<Integer> indexOfOperations = findIndexOfOperation(test);
-  
-  System.out.println(indexOfOperations.get(0));
+  public static List<Integer> findIndexOfOperation(char[] subPhrase) {
+    List<Integer> index = new ArrayList();
 
-  //int sum = charArrayToInt(test, 0, test.length); //get an int value from a chararray
-  //System.out.println(sum);
-
-  System.out.println(findIndexOfOperation(test).get(0));
-
-  for(int j = 0; j < indexOfOperations.size(); j++)
-  {
-    
+    for (int i = 0; i < subPhrase.length; i++) {
+      System.out.print(subPhrase[i]);
+      if (subPhrase[i] == 43 || subPhrase[i] == 42 || subPhrase[i] == 45 || subPhrase[i] == 47) {
+        index.add(i);
+      }
+    }
+    return index;
   }
-  //char[] phraseSub =  phrase.getChars(index[0], index[1], phraseSub, 0);
-  return 1;
-}
 
-public static int charArrayToInt(char []data,int start,int end) throws NumberFormatException
-{
+  public static int findOperations(StringBuilder phrase) {
+
+    int[] index = findIndexOfParenthesis(phrase);
+    phrase.chars();
+    char[] test = new char[index[1] - index[0]]; // filling the char array with the values
+    phrase.getChars(index[0], index[1], test, 0); // get the value need correct index for the intergers
+    System.out.println(test.length);
+    List<Integer> indexOfOperations = findIndexOfOperation(test);
+
+    System.out.println(indexOfOperations.get(0));
+    System.out.println(test.length);
+    // int sum = charArrayToInt(test, 0, test.length); // get an int value from a
+    // chararray
+    // System.out.println(sum);
+
+    System.out.println(findIndexOfOperation(test).get(0));
+
+    for (int j = 0; j < indexOfOperations.size(); j++) {
+
+    }
+    // char[] phraseSub = phrase.getChars(index[0], index[1], phraseSub, 0);
+    return 1;
+  }
+
+  public static int charArrayToInt(char[] data, int start, int end) throws NumberFormatException {
     int result = 0;
-    for (int i = start; i < end; i++)
-    {
-        int digit = (int)data[i] - (int)'0';
-        if ((digit < 0) || (digit > 9)) throw new NumberFormatException();
-        result *= 10;
-        result += digit;
+    for (int i = start; i < end; i++) {
+      int digit = (int) data[i] - (int) '0';
+      if ((digit < 0) || (digit > 9))
+        throw new NumberFormatException();
+      result *= 10;
+      result += digit;
     }
     return result;
-}
+  }
 
-public static int findNumber(char[] number)
-{
-  int number1 = Integer.parseInt(new String(number));
-  int n = 1;
-  return number1;
-}
+  public static int findNumber(char[] number) {
+    int number1 = Integer.parseInt(new String(number));
+    int n = 1;
+    return number1;
+  }
 
 }
--- y.java
+++ y.java
@@ -1,7 +1,121 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.text.html.HTMLDocument.RunElement;
+
 public class y {
-    public static void main(String[] args) 
-    {
-        System.out.println("hello");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String math = input.nextLine();
+        String[] phrase = math.split("\\s+");
+        StringBuilder phraseMut = new StringBuilder();
+
+        for (int i = 0; i < phrase.length; i++) {
+            phraseMut.append(phrase[i]);
+        }
+
+        int[] index = findIndexOfParenthesis(phraseMut);
+        System.out.println(phraseMut.substring(index[0], index[1]));
+        int findOperations = findOperations(phraseMut);
+        System.out.println(findOperations);
     }
-    
+
+    public static int[] findIndexOfParenthesis(StringBuilder phrase) {
+        int[] index = new int[2];
+        for (int i = 0; i < phrase.length(); i++) {
+            char ph = phrase.charAt(i);
+            if (phrase.charAt(i) == 40)
+                index[0] = i + 1; // start parentheses
+            if (phrase.charAt(i) == 41) {
+                index[1] = i; // end parantheses
+                return index;
+            }
+        }
+
+        return index;
+    }
+
+    public static List<Integer> findIndexOfOperation(char[] subPhrase) {
+        List<Integer> index = new ArrayList();
+
+        for (int i = 0; i < subPhrase.length; i++) {
+            System.out.print(subPhrase[i]);
+            if (subPhrase[i] == 43 || subPhrase[i] == 42 || subPhrase[i] == 45 || subPhrase[i] == 47) {
+                index.add(i);
+            }
+        }
+
+        return index;
+    }
+
+    public static int findOperations(StringBuilder phrase) {
+
+        int[] index = findIndexOfParenthesis(phrase);
+        phrase.chars();
+        char[] test = new char[index[1] - index[0]]; // filling the char array with the values
+        phrase.getChars(index[0], index[1], test, 0); // get the value need correct index for the intergers
+
+        List<Integer> indexOfOperations = findIndexOfOperation(test);
+
+        int i = preformeAddition(phrase, indexOfOperations);
+
+        System.out.println(indexOfOperations.get(0));
+
+        System.out.println(findIndexOfOperation(test).get(0));
+
+        for (int j = 0; j < indexOfOperations.size(); j++) {
+
+        }
+        return 1;
+    }
+
+    public static int charArrayToInt(char[] data, int start, int end) throws NumberFormatException {
+        int result = 0;
+        for (int i = start; i < end; i++) {
+            int digit = (int) data[i] - (int) '0';
+            if ((digit < 0) || (digit > 9))
+                throw new NumberFormatException();
+            result *= 10;
+            result += digit;
+        }
+        return result;
+    }
+
+    public static int preformeAddition(StringBuilder phrase, List<Integer> index) {
+        int x = 0;
+        for (int i = index.get(x); x < index.size(); x++) {
+
+            if (x == 0) {
+                char[] RightOfOperatopm = new char[index.get(x + 1) - index.get(x) - 1];
+                char[] LeftOfOperation = new char[index.get(x)];
+                // System.out.println(LeftOfOperation.length);
+                phrase.getChars(index.get(x) + 1, index.get(x + 1), RightOfOperatopm, 0);
+                System.out.println(RightOfOperatopm.length);
+                phrase.getChars(index.get(x + 1), index.get(x), LeftOfOperation, 0);
+
+                // System.out.println(LeftOfOperation.length);
+                // int p2 = RightOfOperatopm.length;
+                // int leftSideide = charArrayToInt(LeftOfOperation, 0, LeftOfOperation.length);
+                // int rightSide = charArrayToInt(LeftOfOperation, 0, LeftOfOperation.length);
+
+            }
+            if (x == index.size() - 1) {
+                continue;
+            }
+            // char[] LeftOfOperation = new char[index.get(x + 1) - index.get(x) - 1];
+            // char[] RightOfOperatopm = new char[index.get(x) - index.get(x - 1) - 1];
+            // phrase.getChars(index.get(x) + 1, index.get(x + 1), RightOfOperatopm, 0);
+            // phrase.getChars(index.get(x - 1), index.get(x) - 1, LeftOfOperation, 0);
+        }
+
+        return x;
+    }
+
+    public static int findNumber(char[] number) {
+        int number1 = Integer.parseInt(new String(number));
+        int n = 1;
+        return number1;
+    }
+
 }
